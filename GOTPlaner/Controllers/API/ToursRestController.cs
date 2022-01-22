1677113c667fef6b143a5585aa2e3f3cd52059c2
arko0x <arkadiusz88084@gmail.com>
2022-01-22 14:39:51 +0100

    Implemented json to c# dto mapping

---
+++ GOTPlaner/Controllers/API/ToursRestController.cs
@@ -0,0 +1,20 @@
+﻿using GOTPlaner.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GOTPlaner.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToursRestController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/api/Tours/Create")]
+        public void AddTour(List<TourItemDto> tourItemDtos)
+        {
+            var result = "";
+        }
+    }
+}
---
+++ GOTPlaner/Models/DTO/TourItemDto.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace GOTPlaner.Models.DTO
+{
+    public class TourItemDto
+    {
+        [JsonProperty("touristPointId")]
+        public int TouristPointId { get; set; }
+
+        [JsonProperty("touristPointName")]
+        public string TouristPointName { get; set; }
+
+        [JsonProperty("ownPoint")]
+        public bool OwnPoint { get; set; }
+
+        [JsonProperty("currentPoints")]
+        public int? CurrentPoints { get; set; }
+        [JsonProperty("numberOfKilometers")]
+        public int? NumberOfKilometers { get; set; }
+        [JsonProperty("levelDifference")]
+        public int? LevelDifference { get; set; }
+
+    }
+}
