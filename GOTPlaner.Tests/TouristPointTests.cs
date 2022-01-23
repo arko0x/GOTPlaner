ffd31a094c08ec96428ee1e10b574344b584b94f
arko0x <arkadiusz88084@gmail.com>
2022-01-23 21:31:44 +0100

    Added some unit tests

---
+++ GOTPlaner.Tests/TouristPointTests.cs
@@ -0,0 +1,47 @@
+﻿using GOTPlaner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GOTPlaner.Tests
+{
+    public class TouristPointTests
+    {
+        [Fact]
+        public void TouristPointsWithSameNameAndDifferentMountainRangesShouldNotBeConsideredEqual()
+        {
+            var pointA = new TouristPoint
+            {
+                Name = "Prełuki",
+                MountainRangeId = MountainRangeId.Bieszczady
+            };
+            var pointB = new TouristPoint
+            {
+                Name = "Prełuki",
+                MountainRangeId = MountainRangeId.PogorzeCiechowickie
+            };
+
+            Assert.False(pointA.Equals(pointB));
+        }
+
+        [Fact]
+        public void TouristPointsWithSameNameAndSameMountainRangesShouldBeConsideredEqual()
+        {
+            var pointA = new TouristPoint
+            {
+                Name = "Prełuki",
+                MountainRangeId = MountainRangeId.Bieszczady
+            };
+            var pointB = new TouristPoint
+            {
+                Name = "Prełuki",
+                MountainRangeId = MountainRangeId.Bieszczady
+            };
+
+            Assert.True(pointA.Equals(pointB));
+        }
+    }
+}
