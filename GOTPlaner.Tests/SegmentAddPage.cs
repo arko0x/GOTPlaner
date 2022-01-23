8ace2167263a2c2f0585ecc9169d6dff2745717c
arko0x <arkadiusz88084@gmail.com>
2022-01-23 19:33:03 +0100

    Added segment addition tests

---
+++ GOTPlaner.Tests/SegmentAddPage.cs
@@ -0,0 +1,98 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOTPlaner.Tests
+{
+    class SegmentAddPage
+    {
+        private readonly IWebDriver _driver;
+        private const string url = "https://localhost:5001/Segment/Create";
+
+        private const string _pointALocator = "PointA";
+        private const string _pointBLocator = "PointB";
+        private const string _pointsABLocator = "PointsAB";
+        private const string _pointsBALocator = "PointsBA";
+        private const string _numberOfKilometersLocator = "NumberOfKilometers";
+        private const string _levelDifferenceLocator = "LevelDifferenceSum";
+        private const string _addSegmentButtonLocator = "/html/body/div/main/div[1]/div/form/div[10]/input";
+
+        public string source => _driver.PageSource;
+
+        private IWebElement _pointA;
+        private IWebElement _pointB;
+        private IWebElement _pointsAB;
+        private IWebElement _pointsBA;
+        private IWebElement _numberOfKilometers;
+        private IWebElement _levelDifference;
+        private IWebElement _addSegmentButton;
+
+        public SegmentAddPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public SegmentAddPage Open()
+        {
+            _driver.Navigate().GoToUrl(url);
+            Initialize();
+            return this;
+        }
+
+        private void Initialize()
+        {
+            _pointA = _driver.FindElement(By.Id(_pointALocator));
+            _pointB = _driver.FindElement(By.Id(_pointBLocator));
+            _pointsAB = _driver.FindElement(By.Id(_pointsABLocator));
+            _pointsBA = _driver.FindElement(By.Id(_pointsBALocator));
+            _numberOfKilometers = _driver.FindElement(By.Id(_numberOfKilometersLocator));
+            _levelDifference = _driver.FindElement(By.Id(_levelDifferenceLocator));
+            _addSegmentButton = _driver.FindElement(By.XPath(_addSegmentButtonLocator));
+        }
+
+        public SegmentAddPage EnterPointA(string value)
+        {
+            _pointA.SendKeys(value);
+            return this;
+        }
+
+        public SegmentAddPage EnterPointB(string value)
+        {
+            _pointB.SendKeys(value);
+            return this;
+        }
+
+        public SegmentAddPage EnterPointsAB(string value)
+        {
+            _pointsAB.SendKeys(value);
+            return this;
+        }
+
+        public SegmentAddPage EnterPointsBA(string value)
+        {
+            _pointsBA.SendKeys(value);
+            return this;
+        }
+
+        public SegmentAddPage EnterNumberOfKilometers(string value)
+        {
+            _numberOfKilometers.SendKeys(value);
+            return this;
+        }
+
+        public SegmentAddPage EnterLevelDifference(string value)
+        {
+            _numberOfKilometers.SendKeys(value);
+            return this;
+        }
+        public SegmentAddPage AddSegment()
+        {
+            _addSegmentButton.Click();
+            return this;
+        }
+
+
+    }
+}
---
+++ GOTPlaner.Tests/SegmentAdditionTests.cs
@@ -0,0 +1,104 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GOTPlaner.Tests
+{
+    public class SegmentAdditionTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        private readonly SegmentAddPage _page;
+
+        public SegmentAdditionTests()
+        {
+            _driver = new ChromeDriver();
+            _page = new SegmentAddPage(_driver);
+            _page.Open();
+        }
+
+        [Fact]
+        public void WhenNoPointAThenErrorIsShown()
+        {
+            _page
+                .EnterPointB("value")
+                .EnterPointsAB("10")
+                .EnterPointsBA("10")
+                .EnterLevelDifference("100")
+                .EnterNumberOfKilometers("10");
+
+            _page.AddSegment();
+
+            Assert.Contains("The PointA field is required", _page.source);
+        }
+
+        [Fact]
+        public void WhenNoPointBThenErrorIsShown()
+        {
+            _page
+                .EnterPointA("value")
+                .EnterPointsAB("10")
+                .EnterPointsBA("10")
+                .EnterLevelDifference("100")
+                .EnterNumberOfKilometers("10");
+
+            _page.AddSegment();
+
+            Assert.Contains("The PointB field is required", _page.source);
+        }
+
+        [Fact]
+        public void WhenNoPointsABThenErrorIsShown()
+        {
+            _page
+                .EnterPointB("value")
+                .EnterPointA("otherValue")
+                .EnterPointsBA("10")
+                .EnterLevelDifference("100")
+                .EnterNumberOfKilometers("10");
+
+            _page.AddSegment();
+
+            Assert.Contains("The PointsAB field is required", _page.source);
+        }
+
+        [Fact]
+        public void WhenNoNumberOfKilometersThenErrorIsShown()
+        {
+            _page
+                .EnterPointA("otherValue")
+                .EnterPointB("value")
+                .EnterPointsAB("10")
+                .EnterPointsBA("10")
+                .EnterLevelDifference("100");
+
+            _page.AddSegment();
+
+            Assert.Contains("The NumberOfKilometers field is required", _page.source);
+        }
+
+        [Fact]
+        public void WhenNoLevelDifferenceThenErrorIsShown()
+        {
+            _page
+                .EnterPointA("otherValue")
+                .EnterPointB("value")
+                .EnterPointsAB("10")
+                .EnterPointsBA("10")
+                .EnterNumberOfKilometers("10");
+
+            _page.AddSegment();
+
+            Assert.Contains("The LevelDifferenceSum field is required", _page.source);
+        }
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
